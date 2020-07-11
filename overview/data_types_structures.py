6c73ede18b07303937b948fab0e406aef49a40c9
stevekutz <stkutz@gmail.com>
2020-07-10 21:34:31 -0500

    added exp with dict & methods

--- overview/data_types_structures.py
+++ overview/data_types_structures.py
@@ -54,92 +54,139 @@
 # are better for managing elements that are different data types
 # lists are better when of same type
 
-tupl1 = (True, False)
-print(f' tupl1 = {tupl1}')
+# tupl1 = (True, False)
+# print(f' tupl1 = {tupl1}')
 
-# tupl1 variable can be re-asssigned
-tupl1 = (False, True)
-print(f' tupl1 = {tupl1}') 
+# # tupl1 variable can be re-asssigned
+# tupl1 = (False, True)
+# print(f' tupl1 = {tupl1}') 
 
-# using tuple to swap
-x,y = tupl1
-print(f' original x: {x}   y: {y} ')
+# # using tuple to swap
+# x,y = tupl1
+# print(f' original x: {x}   y: {y} ')
 
-x,y = y,x
-print(f' swapped is x: {x}   y: {y} ')
-
-
-
-# can be declared without parenthesis, called `packing`
-tupl2 = 1, 2, 'A'
-
-# unpacking a tuple
-val1, val2, val3 = tupl2
-print(f' val1: {val1}  val2: {val2}  val3: {val3}')  # val1: 1  val2: 2  val3: A
-
-# concatenating a tuple
-tupl3 = tupl1 + tupl2
-print(f'tupl3 is {tupl3}')   # tupl3 is (False, True, 1, 2, 'A')
-
-# concatenating a tuple with zip, use the tuple() function to read the tuple
-new_tuple = zip(tupl1, tupl2)
-print(f' concatenated tuple using zip {tuple(new_tuple)}')
-
-# slicing a tuple
-for val in tupl3[3:5]:
-    print(f' val is {val}')
-#  val is 2
-#  val is A
-
-# declaring with only 1 element, MUST USE COMMA
-not_tuple = ("value")
-print(f' not_tuple type is {type(not_tuple)} ')
-
-true_tuple = ("value", )
-print(f' true_tuple type is {type(true_tuple)} ')
-
-# nested tuples
-nested_tuple = (11, 200, ('a', 'b', 'c'))
-
-for val in nested_tuple:
-    print(f' not nested is: {val} ')
-    if isinstance(val, tuple):
-        for nested in val:
-            print(f'  \t nested val is {nested}')        
-
-#  not nested is: 11 
-#  not nested is: 200 
-#  not nested is: ('a', 'b', 'c') 
-#          nested val is a
-#          nested val is b
-#          nested val is c
+# x,y = y,x
+# print(f' swapped is x: {x}   y: {y} ')
 
 
-# accessing with index
-print(f' val at index 1 is {nested_tuple[1]} of tuple length {len(nested_tuple)}')
-#  val at index 1 is 200 of length 3
 
-print(f' val at index -1 is the last item {nested_tuple[-1]}')  # val at index -1 is the last item ('a', 'b', 'c')
+# # can be declared without parenthesis, called `packing`
+# tupl2 = 1, 2, 'A'
 
-# finding index of tuple element value
-print(f' index of tuple element 200 is {nested_tuple.count(200)}')    # 1
+# # unpacking a tuple
+# val1, val2, val3 = tupl2
+# print(f' val1: {val1}  val2: {val2}  val3: {val3}')  # val1: 1  val2: 2  val3: A
 
-# verify is value is in tuple
-print(f' 5 is in nested_tuple {5 in nested_tuple}')    # False
+# # concatenating a tuple
+# tupl3 = tupl1 + tupl2
+# print(f'tupl3 is {tupl3}')   # tupl3 is (False, True, 1, 2, 'A')
 
-# comparing tuples
+# # concatenating a tuple with zip, use the tuple() function to read the tuple
+# new_tuple = zip(tupl1, tupl2)
+# print(f' concatenated tuple using zip {tuple(new_tuple)}')
 
-tup1 = ( 1, 14)
-tup2 = ( 2, 18)
-tup3 = (1, 8, 1)
-tup4 = (1, 14, 1)
+# # slicing a tuple
+# for val in tupl3[3:5]:
+#     print(f' val is {val}')
+# #  val is 2
+# #  val is A
 
-print(f' tup1 > tup2  {tup1 > tup2}')   # False 14 is not > 18
-print(f' tup1 > tup3  {tup1 > tup3}')   # True, because 2nd elem  14 > 8, length of rest does not matter
-print(f' tup1 > tup4  {tup1 > tup4}')   # False because all elements match but tup4 has more elements
+# # declaring with only 1 element, MUST USE COMMA
+# not_tuple = ("value")
+# print(f' not_tuple type is {type(not_tuple)} ')
 
-for item1, item2 in tup1, tup2:
-    print(f'item 1 > item2 {item1 > item2}')
-  # this does not work as planned,
-  # item1, item2 are unpacked from tup1 and compared   1 > 14    False
-  # item1, item2 are then unpacked from tup2 and compared 2 > 18   False
+# true_tuple = ("value", )
+# print(f' true_tuple type is {type(true_tuple)} ')
+
+# # nested tuples
+# nested_tuple = (11, 200, ('a', 'b', 'c'))
+
+# for val in nested_tuple:
+#     print(f' not nested is: {val} ')
+#     if isinstance(val, tuple):
+#         for nested in val:
+#             print(f'  \t nested val is {nested}')        
+
+# #  not nested is: 11 
+# #  not nested is: 200 
+# #  not nested is: ('a', 'b', 'c') 
+# #          nested val is a
+# #          nested val is b
+# #          nested val is c
+
+
+# # accessing with index
+# print(f' val at index 1 is {nested_tuple[1]} of tuple length {len(nested_tuple)}')
+# #  val at index 1 is 200 of length 3
+
+# print(f' val at index -1 is the last item {nested_tuple[-1]}')  # val at index -1 is the last item ('a', 'b', 'c')
+
+# # finding index of tuple element value
+# print(f' index of tuple element 200 is {nested_tuple.count(200)}')    # 1
+
+# # verify is value is in tuple
+# print(f' 5 is in nested_tuple {5 in nested_tuple}')    # False
+
+# # comparing tuples
+
+# tup1 = ( 1, 14)
+# tup2 = ( 2, 18)
+# tup3 = (1, 8, 1)
+# tup4 = (1, 14, 1)
+
+# print(f' tup1 > tup2  {tup1 > tup2}')   # False 14 is not > 18
+# print(f' tup1 > tup3  {tup1 > tup3}')   # True, because 2nd elem  14 > 8, length of rest does not matter
+# print(f' tup1 > tup4  {tup1 > tup4}')   # False because all elements match but tup4 has more elements
+
+# for item1, item2 in tup1, tup2:
+#     print(f'item 1 > item2 {item1 > item2}')
+#   # this does not work as planned,
+#   # item1, item2 are unpacked from tup1 and compared   1 > 14    False
+#   # item1, item2 are then unpacked from tup2 and compared 2 > 18   False
+
+
+############################################
+# dictionaries - stoes seq of mapped key:value pairs
+# the ONLY built in mapping type
+#  `in` with dictionaries uses a hashing function and searches very quickly
+
+## WAYS TO CREATE dictionary
+# using zip to create a dict
+dict1 = dict(zip('HelloWorld', range(10)))
+print(dict1)     # {'H': 0, 'e': 1, 'l': 8, 'o': 6, 'W': 5, 'r': 7, 'd': 9}
+
+# directly declare dict
+dict2 = {'first': '1', 'second': '2', 'third': '3', 'fourth': '4'}
+print(dict2)   # {'first': '1', 'second': '2', 'third': '3', 'fourth': '4'}
+
+# use the `dict` keyword
+dict3 = dict({'one': '1', 'two': '2', 'three': '3'})
+print(dict3)     # {'one': '1', 'two': '2', 'three': '3'}
+
+# APPEND to dict3
+dict3['four'] = 4
+print(f' Appended  dict3  {dict3} ')  # Appended  dict3  {'one': '1', 'two': '2', 'three': '3', 'four': 4}
+
+# use UPDATE to ADD MULTIPLE key:value pairs to dict3
+dict3.update({'five': 5, 'six': 6})
+print(f' update adds kv pairs {dict3}')  # update adds kv pairs {'one': '1', 'two': '2', 'three': '3', 'four': 4, 'five': 5, 'six': 6}
+
+# use UPDATE change a value in dict
+dict3.update(six = -66)
+print(f" six changed {dict3['six']}")    # six changed -66
+
+# Verify if key exists (does not work for values)
+#  `in` with dictionaries uses a hashing function and searches very quickly
+print(f" {'two' in dict3}   ")     # True
+print(f" {'nine' in dict3} ")      # False 
+
+# COPY by REFERENCE (not same memory location)
+dict5 = dict3.copy()
+print(f' id dict1: {id(dict1)} id dict5: {id(dict5)}')  # should show different mem location
+
+# Creates new dict from defined seq of keys and intializes to new value
+keys = {'a', 'b', 'c'}
+
+dict6 = dict.fromkeys(keys, 0)
+print(dict6)   # {'b': 0, 'c': 0, 'a': 0}
+
