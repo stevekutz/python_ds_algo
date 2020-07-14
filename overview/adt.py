48b24bbae25b240a9b1bb1a30ace9c258d3b48a4
stevekutz <stkutz@gmail.com>
2020-07-13 22:00:26 -0500

    added Counter methods

--- overview/adt.py
+++ overview/adt.py
@@ -79,199 +79,251 @@ import itertools, collections, string
 # A ChainMap makes a good “context” container, since it can be treated as a stack 
 # for which changes happen as the stack grows, with these changes being discarded again as the stack shrinks
 
-dict1 = {'a': 1, 'b': 2, 'c': 3}
-dict2 = {'d': 4, 'e': 5}
+# dict1 = {'a': 1, 'b': 2, 'c': 3}
+# dict2 = {'d': 4, 'e': 5}
 
-# Create Chainmap
-## if we imported as `from collections import deque, ChainMap`
-## wwe would declare as chain_map0 = ChainMap(dict1)
+# # Create Chainmap
+# ## if we imported as `from collections import deque, ChainMap`
+# ## wwe would declare as chain_map0 = ChainMap(dict1)
 
-chain_map0 = collections.ChainMap(dict1)
-print(f' chain_map0 is {chain_map0.maps}')
+# chain_map0 = collections.ChainMap(dict1)
+# print(f' chain_map0 is {chain_map0.maps}')
 
-chain_map = collections.ChainMap(dict1, dict2)
-print(f'chain_map is {chain_map}')  # chain_map is ChainMap({'a': 1, 'b': 2, 'c': 3}, {'d': 4, 'e': 5})
-
-print(f' \t >>>  dict1 is {dict1}')
-
-# return a list of ChainMap mappings
-print(chain_map.maps)   # [{'a': 1, 'b': 2, 'c': 3}, {'d': 4, 'e': 5}]
-
-# return all keys
-print(chain_map.keys())    # KeysView(ChainMap({'a': 1, 'b': 2, 'c': 3}, {'d': 4, 'e': 5}))
-
-# return all keys in a list
-print(list(chain_map.keys()))  # ['d', 'e', 'a', 'b', 'c']
-
-# return all values
-print(chain_map.values())  # ValuesView(ChainMap({'a': 1, 'b': 2, 'c': 3}, {'d': 4, 'e': 5}))
-
-print(f' \t >>>  dict1 is {dict1}')
-
-# return all values in a list
-print(list(chain_map.values()))    # [4, 5, 1, 2, 3]clear
-
-# print out first index
-print(f' first value in chain_map is {chain_map.maps[0]} ')
-
-print(f' \t >>>  dict1 is {dict1}')
-
-# change value in first mapping  {'a': 100, 'b': 2, 'c': 3}
-# only updates/deletes can be done to first mapping
-chain_map['a'] = 100
-print(f' changed first chain value  {chain_map.maps}')    # [{'a': 100, 'b': 2, 'c': 3}, {'d': 4, 'e': 5}]
-
-
-print(f' \t >>>  dict1 is {dict1}')
-
-
-# try to change value in second mapping
-##  >>   This appends to first chain and does not update second chain key of 'e'
-##  >>   A ChainMap incorporates the underlying mappings by reference, e.g.  copies mem location
-##  This will also update `dict_4`
-
-
-# chain_map['e'] = 40
-# print(f' tried to change second chain {chain_map.maps}')  # tried to change second chain [{'a': 100, 'b': 2, 'c': 3, 'e': 40}, {'d': 4, 'e': 5}]
-# print(f' \t\t\t >>>  dict1 is {dict1}')    # {'a': 100, 'b': 2, 'c': 3, 'e': 40}
-
-# create child ChainMap with value that overrides parent
-chain_map2 = chain_map.new_child({'a': -999})
-print(chain_map2.maps)
+# chain_map = collections.ChainMap(dict1, dict2)
+# print(f'chain_map is {chain_map}')  # chain_map is ChainMap({'a': 1, 'b': 2, 'c': 3}, {'d': 4, 'e': 5})
 
 # print(f' \t >>>  dict1 is {dict1}')
 
-# reverse child ChainMap
-print(f' \t >>>  dict1 is {dict1}')
-chain_map3 = collections.ChainMap(dict1, dict2)
-print(f' chain_map3 is {chain_map3}')    # chain_map3 is ChainMap({'a': 100, 'b': 2, 'c': 3, 'e': 40}, {'d': 4, 'e': 5})
+# # return a list of ChainMap mappings
+# print(chain_map.maps)   # [{'a': 1, 'b': 2, 'c': 3}, {'d': 4, 'e': 5}]
 
-chain_map3.maps = reversed(chain_map3.maps)
-print(f'should be reversed {(chain_map3)}')    # should be reversed ChainMap({'d': 4, 'e': 5}, {'a': 100, 'b': 2, 'c': 3, 'e': 40})
+# # return all keys
+# print(chain_map.keys())    # KeysView(ChainMap({'a': 1, 'b': 2, 'c': 3}, {'d': 4, 'e': 5}))
 
-print("Reversed Chain: " + str(chain_map3))
+# # return all keys in a list
+# print(list(chain_map.keys()))  # ['d', 'e', 'a', 'b', 'c']
 
-dict3 = {"A": "apple", "B": "beetle", "C": "cow"}
-chain_map4 = collections.ChainMap(dict3)
-print(chain_map4)   # ChainMap({'A': 'apple', 'B': 'beetle', 'C': 'cow'}
+# # return all values
+# print(chain_map.values())  # ValuesView(ChainMap({'a': 1, 'b': 2, 'c': 3}, {'d': 4, 'e': 5}))
 
-chain_map4 = chain_map4.new_child({"E": "egg"})
-print(f' added E: egg  {chain_map4}')     # ChainMap({'E': 'egg'}, {'A': 'apple', 'B': 'beetle', 'C': 'cow'})
+# print(f' \t >>>  dict1 is {dict1}')
 
-chain_map4.maps = reversed(chain_map4.maps)
+# # return all values in a list
+# print(list(chain_map.values()))    # [4, 5, 1, 2, 3]clear
 
-print(f' REVERSED !!!   {chain_map4}')
-print("Reversed Chain: " + str(chain_map4))     # ChainMap({'A': 'apple', 'B': 'beetle', 'C': 'cow'}, {'E': 'egg'})
+# # print out first index
+# print(f' first value in chain_map is {chain_map.maps[0]} ')
 
-con_code1 = {'India' : 'IN', 'China' : 'CN'}
-con_code2 = {'France' : 'FR', 'United Kingdom' : 'GB'}
-code = {'Japan' : 'JP'}
+# print(f' \t >>>  dict1 is {dict1}')
 
-chain = collections.ChainMap(con_code1, con_code2)
-print("Initial Chain: " + str(chain.maps))
-# Initial Chain: [{'India': 'IN', 'China': 'CN'}, {'France': 'FR', 'United Kingdom': 'GB'}]
+# # change value in first mapping  {'a': 100, 'b': 2, 'c': 3}
+# # only updates/deletes can be done to first mapping
+# chain_map['a'] = 100
+# print(f' changed first chain value  {chain_map.maps}')    # [{'a': 100, 'b': 2, 'c': 3}, {'d': 4, 'e': 5}]
 
 
-chain = chain.new_child(code)    #Insert New Child
-print("Chain after Inserting new Child: " + str(chain.maps))
-# Chain after Inserting new Child: [{'Japan': 'JP'}, {'India': 'IN', 'China': 'CN'}, {'France': 'FR', 'United Kingdom': 'GB'}]
-
-chain.maps = reversed(chain.maps)
-print("Reversed Chain: " + str(chain))   
-# Reversed Chain: ChainMap({'France': 'FR', 'United Kingdom': 'GB'}, {'India': 'IN', 'China': 'CN'}, {'Japan': 'JP'})
+# print(f' \t >>>  dict1 is {dict1}')
 
 
-
-####  Since linking involves mapping by reference
-# updating `dict_4` will update `new_chain`
-# AND updating `new_chain` will update `dict`
-dict_4 = {"One": 1, "Two": 2}
-dict_5 = {"Five": 5, "Six": 6}
-
-new_chain = collections.ChainMap(dict_4)
-print(f' new chain is {dict_4}')
-
-new_chain["three"] = 3
-
-print(f'new_chain is {dict_4} ')   # new_chain is {'One': 1, 'Two': 2, 'three': 3} 
-print(f' dict_4 is {dict_4}')      # dict_4 is {'One': 1, 'Two': 2, 'three': 3}
+# # try to change value in second mapping
+# ##  >>   This appends to first chain and does not update second chain key of 'e'
+# ##  >>   A ChainMap incorporates the underlying mappings by reference, e.g.  copies mem location
+# ##  This will also update `dict_4`
 
 
-new_chain["four"] = 4
-print(f'new_chain is {dict_4} ')    # new_chain is {'One': 1, 'Two': 2, 'three': 3, 'four': 4} 
-print(f' dict_4 is {dict_4}')      #  dict_4 is {'One': 1, 'Two': 2, 'three': 3, 'four': 4}
+# # chain_map['e'] = 40
+# # print(f' tried to change second chain {chain_map.maps}')  # tried to change second chain [{'a': 100, 'b': 2, 'c': 3, 'e': 40}, {'d': 4, 'e': 5}]
+# # print(f' \t\t\t >>>  dict1 is {dict1}')    # {'a': 100, 'b': 2, 'c': 3, 'e': 40}
 
-dict_4["FIVE"] = 5
-print(f'new_chain is {dict_4} ')     # new_chain is {'One': 1, 'Two': 2, 'three': 3, 'four': 4, 'FIVE': 5}
-print(f' dict_4 is {dict_4}')        # dict_4 is {'One': 1, 'Two': 2, 'three': 3, 'four': 4, 'FIVE': 5}
+# # create child ChainMap with value that overrides parent
+# chain_map2 = chain_map.new_child({'a': -999})
+# print(chain_map2.maps)
 
+# # print(f' \t >>>  dict1 is {dict1}')
 
-new_chain2 = collections.ChainMap(dict_4, dict_5)
-print(f' list new_chain2 {new_chain2} ')
-# list new_chain2 ChainMap({'One': 1, 'Two': 2, 'three': 3, 'four': 4, 'FIVE': 5}, {'Five': 5, 'Six': 6}) 
+# # reverse child ChainMap
+# print(f' \t >>>  dict1 is {dict1}')
+# chain_map3 = collections.ChainMap(dict1, dict2)
+# print(f' chain_map3 is {chain_map3}')    # chain_map3 is ChainMap({'a': 100, 'b': 2, 'c': 3, 'e': 40}, {'d': 4, 'e': 5})
 
-# `parents` skips over original map
-parent_chain = new_chain2.parents
-print(f' parent_chain {parent_chain}')   # parent_chain ChainMap({'Five': 5, 'Six': 6})
+# chain_map3.maps = reversed(chain_map3.maps)
+# print(f'should be reversed {(chain_map3)}')    # should be reversed ChainMap({'d': 4, 'e': 5}, {'a': 100, 'b': 2, 'c': 3, 'e': 40})
 
+# print("Reversed Chain: " + str(chain_map3))
 
-# popping values from the first mapping, will not see children
-# popped = new_chain2.pop("One")
-# print(f' popped is {popped}')    # popped is 1
-# print(f' new_chain2 is {new_chain2}')   # new_chain2 is ChainMap({'Two': 2, 'three': 3, 'four': 4, 'FIVE': 5}, {'Five': 5, 'Six': 6})
+# dict3 = {"A": "apple", "B": "beetle", "C": "cow"}
+# chain_map4 = collections.ChainMap(dict3)
+# print(chain_map4)   # ChainMap({'A': 'apple', 'B': 'beetle', 'C': 'cow'}
 
-# removes item from end of first mapping
-popped = new_chain2.popitem()
-print(f' popped is {popped}')
-print(f' new_chain2 is {new_chain2} ') # new_chain2 is ChainMap({'One': 1, 'Two': 2, 'three': 3, 'four': 4}, {'Five': 5, 'Six': 6}) 
+# chain_map4 = chain_map4.new_child({"E": "egg"})
+# print(f' added E: egg  {chain_map4}')     # ChainMap({'E': 'egg'}, {'A': 'apple', 'B': 'beetle', 'C': 'cow'})
 
+# chain_map4.maps = reversed(chain_map4.maps)
 
-class ReadNestedChain(collections.ChainMap):
-    " Read/Write into nested mappings"
+# print(f' REVERSED !!!   {chain_map4}')
+# print("Reversed Chain: " + str(chain_map4))     # ChainMap({'A': 'apple', 'B': 'beetle', 'C': 'cow'}, {'E': 'egg'})
 
-    def __setitem__(self, key, value):
-        for mapping in self.maps:
-            if key in mapping:
-                mapping[key] = value
-                return
-        self.maps[0][key] = value    
+# con_code1 = {'India' : 'IN', 'China' : 'CN'}
+# con_code2 = {'France' : 'FR', 'United Kingdom' : 'GB'}
+# code = {'Japan' : 'JP'}
 
-
-    def __delitem__(self, key):
-        for mapping in self.maps:
-            if key in mapping:
-                del mapping[key]
-                return
-        # raise KeyError(key)
-        print(f' key \"{key}\"  does not exist ')            
+# chain = collections.ChainMap(con_code1, con_code2)
+# print("Initial Chain: " + str(chain.maps))
+# # Initial Chain: [{'India': 'IN', 'China': 'CN'}, {'France': 'FR', 'United Kingdom': 'GB'}]
 
 
-deep_nested_chain = ReadNestedChain({'One': '1', 'Two': '2', 'three': '3', 'four': '4'}, {'Five': '5', 'Six': '6'}, {'eight': '8', 'nine': '9'})
-print(f' {deep_nested_chain}')
+# chain = chain.new_child(code)    #Insert New Child
+# print("Chain after Inserting new Child: " + str(chain.maps))
+# # Chain after Inserting new Child: [{'Japan': 'JP'}, {'India': 'IN', 'China': 'CN'}, {'France': 'FR', 'United Kingdom': 'GB'}]
 
-# we can tell if a value is in the ChainMap but can only modify first mapping
-print({"nine" in deep_nested_chain})   # True
+# chain.maps = reversed(chain.maps)
+# print("Reversed Chain: " + str(chain))   
+# # Reversed Chain: ChainMap({'France': 'FR', 'United Kingdom': 'GB'}, {'India': 'IN', 'China': 'CN'}, {'Japan': 'JP'})
 
-deep_nested_chain2 = ReadNestedChain(deep_nested_chain)
-print(f' {deep_nested_chain2}')
-# ReadNestedChain(ReadNestedChain({'One': '1', 'Two': '2', 'three': '3', 'four': '4'}, {'Five': '5', 'Six': '6'}, {'eight': '8', 'nine': '9'}))
 
-# change key value that is two levels down
-deep_nested_chain2["nine"] = 999
-print(f' {deep_nested_chain2}')
-# ReadNestedChain(ReadNestedChain({'One': '1', 'Two': '2', 'three': '3', 'four': '4'}, {'Five': '5', 'Six': '6'}, {'eight': '8', 'nine': 999}))
 
-# append to first mapping
-deep_nested_chain2["Zero"] = 0
-print(f' {deep_nested_chain2}')
-# ReadNestedChain(ReadNestedChain({'One': '1', 'Two': '2', 'three': '3', 'four': '4', 'Zero': 0}, {'Five': '5', 'Six': '6'}, {'eight': '8', 'nine': 999}))
+# ####  Since linking involves mapping by reference
+# # updating `dict_4` will update `new_chain`
+# # AND updating `new_chain` will update `dict`
+# dict_4 = {"One": 1, "Two": 2}
+# dict_5 = {"Five": 5, "Six": 6}
 
-# try to delete key that does not exist 
-del deep_nested_chain2["twelve"]    #  key "twelve"  does not exist 
+# new_chain = collections.ChainMap(dict_4)
+# print(f' new chain is {dict_4}')
 
-# delete key that exsits
-del deep_nested_chain2["nine"]
-print(f' after del {deep_nested_chain2}')
+# new_chain["three"] = 3
 
-# for item in deep_nested_chain2.items():
-#     print(item)
+# print(f'new_chain is {dict_4} ')   # new_chain is {'One': 1, 'Two': 2, 'three': 3} 
+# print(f' dict_4 is {dict_4}')      # dict_4 is {'One': 1, 'Two': 2, 'three': 3}
+
+
+# new_chain["four"] = 4
+# print(f'new_chain is {dict_4} ')    # new_chain is {'One': 1, 'Two': 2, 'three': 3, 'four': 4} 
+# print(f' dict_4 is {dict_4}')      #  dict_4 is {'One': 1, 'Two': 2, 'three': 3, 'four': 4}
+
+# dict_4["FIVE"] = 5
+# print(f'new_chain is {dict_4} ')     # new_chain is {'One': 1, 'Two': 2, 'three': 3, 'four': 4, 'FIVE': 5}
+# print(f' dict_4 is {dict_4}')        # dict_4 is {'One': 1, 'Two': 2, 'three': 3, 'four': 4, 'FIVE': 5}
+
+
+# new_chain2 = collections.ChainMap(dict_4, dict_5)
+# print(f' list new_chain2 {new_chain2} ')
+# # list new_chain2 ChainMap({'One': 1, 'Two': 2, 'three': 3, 'four': 4, 'FIVE': 5}, {'Five': 5, 'Six': 6}) 
+
+# # `parents` skips over original map
+# parent_chain = new_chain2.parents
+# print(f' parent_chain {parent_chain}')   # parent_chain ChainMap({'Five': 5, 'Six': 6})
+
+
+# # popping values from the first mapping, will not see children
+# # popped = new_chain2.pop("One")
+# # print(f' popped is {popped}')    # popped is 1
+# # print(f' new_chain2 is {new_chain2}')   # new_chain2 is ChainMap({'Two': 2, 'three': 3, 'four': 4, 'FIVE': 5}, {'Five': 5, 'Six': 6})
+
+# # removes item from end of first mapping
+# popped = new_chain2.popitem()
+# print(f' popped is {popped}')
+# print(f' new_chain2 is {new_chain2} ') # new_chain2 is ChainMap({'One': 1, 'Two': 2, 'three': 3, 'four': 4}, {'Five': 5, 'Six': 6}) 
+
+
+# class ReadNestedChain(collections.ChainMap):
+#     " Read/Write into nested mappings"
+
+#     def __setitem__(self, key, value):
+#         for mapping in self.maps:
+#             if key in mapping:
+#                 mapping[key] = value
+#                 return
+#         self.maps[0][key] = value    
+
+
+#     def __delitem__(self, key):
+#         for mapping in self.maps:
+#             if key in mapping:
+#                 del mapping[key]
+#                 return
+#         # raise KeyError(key)
+#         print(f' key \"{key}\"  does not exist ')            
+
+
+# deep_nested_chain = ReadNestedChain({'One': '1', 'Two': '2', 'three': '3', 'four': '4'}, {'Five': '5', 'Six': '6'}, {'eight': '8', 'nine': '9'})
+# print(f' {deep_nested_chain}')
+
+# # we can tell if a value is in the ChainMap but can only modify first mapping
+# print({"nine" in deep_nested_chain})   # True
+
+# deep_nested_chain2 = ReadNestedChain(deep_nested_chain)
+# print(f' {deep_nested_chain2}')
+# # ReadNestedChain(ReadNestedChain({'One': '1', 'Two': '2', 'three': '3', 'four': '4'}, {'Five': '5', 'Six': '6'}, {'eight': '8', 'nine': '9'}))
+
+# # change key value that is two levels down
+# deep_nested_chain2["nine"] = 999
+# print(f' {deep_nested_chain2}')
+# # ReadNestedChain(ReadNestedChain({'One': '1', 'Two': '2', 'three': '3', 'four': '4'}, {'Five': '5', 'Six': '6'}, {'eight': '8', 'nine': 999}))
+
+# # append to first mapping
+# deep_nested_chain2["Zero"] = 0
+# print(f' {deep_nested_chain2}')
+# # ReadNestedChain(ReadNestedChain({'One': '1', 'Two': '2', 'three': '3', 'four': '4', 'Zero': 0}, {'Five': '5', 'Six': '6'}, {'eight': '8', 'nine': 999}))
+
+# # try to delete key that does not exist 
+# del deep_nested_chain2["twelve"]    #  key "twelve"  does not exist 
+
+# # delete key that exsits
+# del deep_nested_chain2["nine"]
+# print(f' after del {deep_nested_chain2}')
+
+# # for item in deep_nested_chain2.items():
+# #     print(item)
+
+
+##################################################################
+## Counter is a dict subclas with keys that are hashable obj
+
+
+# Initialize Counter by passing in seq obj
+print(collections.Counter("this is a the way it works"))
+# Counter({' ': 6, 't': 3, 'i': 3, 's': 3, 'h': 2, 'a': 2, 'w': 2, 'e': 1, 'y': 1, 'o': 1, 'r': 1, 'k': 1})
+
+count_1 = collections.Counter("this is a the way it works")
+print(count_1)
+
+# Initialize Counter by passing in dict
+dict_6 = {'One': '1', 'Two': '2', 'three': '3', 'four': '4'}
+count_2 = collections.Counter(dict_6)
+print(count_2)
+
+# Initialize Counter by passing in tuple
+count_3 = collections.Counter(a = 1, b = 2, c = 3)
+print(f' count_3 is  {count_3}')    # count_3 is  Counter({'c': 3, 'b': 2, 'a': 1})
+
+# Create empty Counter obj
+count_4 = collections.Counter()
+print(count_4)
+
+# update values
+count_4.update("this just works like it should")
+print(count_4)
+# Counter({' ': 5, 's': 4, 't': 3, 'i': 3, 'h': 2, 'u': 2, 'o': 2, 'k': 2, 'l': 2, 'j': 1, 'w': 1, 'r': 1, 'e': 1, 'd': 1})
+
+# modify values
+count_4[' '] = 10
+count_4['s'] = 100
+print(count_4)
+# Counter({'s': 100, ' ': 10, 't': 3, 'i': 3, 'h': 2, 'u': 2, 'o': 2, 'k': 2, 'l': 2, 'j': 1, 'w': 1, 'r': 1, 'e': 1, 'd': 1})
+
+# use update to add to several values, NOTE what happens to `t`
+count_4.update({'s':20, ' ':40, 't':-60})
+print(count_4)
+# Counter({'s': 120, ' ': 50, 'i': 3, 'h': 2, 'u': 2, 'o': 2, 'k': 2, 'l': 2, 'j': 1, 'w': 1, 'r': 1, 'e': 1, 'd': 1, 't': -57})
+
+# only show positive count values, sorted is added since `elements` does not always return in order
+# count_3 is  Counter({'c': 3, 'b': 2, 'a': 1})
+print(sorted(count_3.elements()))   # ['a', 'b', 'b', 'c', 'c', 'c']
+
+# most_common
+print(count_3.most_common())    # [('c', 3), ('b', 2), ('a', 1)]
+
+# `subtract` reduces count values
+print(count_3.subtract({'c': 2}))   # None
+print(count_3)    # Counter({'b': 2, 'a': 1, 'c': 1})
